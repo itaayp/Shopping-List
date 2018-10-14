9a81a2d7869a5892d3a2d6adbb227733cab864d6
Itay Peceniski <itay.profissional@gmail.com>
2018-10-14 13:12:16 -0300

    Shopping List App done

---
+++ main.js
@@ -0,0 +1,46 @@
+var shoppingList = new Vue({
+    el: '#shopping-list',
+    data: {
+        header: 'Vue is ready to party',
+        newItem: '',
+        state: 'default',
+        items : [
+            {
+                label: '10 party hats',
+                purchased: false,
+                highPriority: false
+            },
+            {
+                label: '2 board games',
+                purchased: true,
+                highPriority: false
+            },
+            {
+                label: '20 cups',
+                purchased: false,
+                highPriority: true
+            }
+        ]
+    },
+    computed: {
+        reversedItems() {
+            return this.items.slice(0).reverse();
+        }
+    },
+    methods: {
+        saveItem: function() {
+            this.items.push({
+                label: this.newItem, purchased: false
+            });
+            this.newItem = '';
+        },
+        changeState: function(newState) {
+            this.state = newState;
+
+            this.newItem = '';
+        },
+        togglePurchased: function(item) {
+            item.purchased = !item.purchased;   
+        }
+    }
+});
